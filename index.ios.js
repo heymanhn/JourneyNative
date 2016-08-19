9a5874c764a97d651cba60dbcac9a6f84199bae9
Herman Ng <heymanhn@gmail.com>
2016-08-19 02:07:28 -0700

    Add signup and login buttons to welcome screen
    [Finishes #128201849]

--- index.ios.js
+++ index.ios.js
@@ -11,17 +11,29 @@ import {
   Text,
   View
 } from 'react-native';
+import Button from 'react-native-button';
 
 class JourneyNative extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>
-          Journey
-        </Text>
-        <Text style={styles.tagline}>
-          Realize your dreams
-        </Text>
+        <View style={styles.titles}>
+          <Text style={styles.title}>
+            Journey
+          </Text>
+          <Text style={styles.tagline}>
+            Realize your dreams
+          </Text>
+        </View>
+        <View style={styles.buttons}>
+          <Button style={styles.button}>
+            Sign up
+          </Button>
+          <Button style={styles.button}>
+            Log in
+          </Button>
+        </View>
+
       </View>
     );
   }
@@ -34,18 +46,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#ffe945',
   },
+  titles: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
   title: {
     fontSize: 48,
     fontWeight: '200',
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   tagline: {
     fontSize: 24,
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
+    marginBottom: 5
   },
+  buttons: {
+    flex: 1,
+    marginTop: 50
+  },
+  button: {
+    width: 150,
+    borderWidth: 2,
+    padding: 10,
+    fontWeight: '100',
+    color: '#333333',
+    fontSize: 18,
+    margin: 10
+  }
 });
 
 AppRegistry.registerComponent('JourneyNative', () => JourneyNative);
