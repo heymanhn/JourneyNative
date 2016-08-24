ce8dcf8e746fbd7641998b3d12eae547dd13b390
Herman Ng <heymanhn@gmail.com>
2016-08-24 14:33:05 -0700

    Create shared component `ScreenWithInput`
    The login and signup screens will share this component. [#128201889]

--- app/components/LoginEmailScreen.js
+++ app/components/LoginEmailScreen.js
@@ -1,73 +1,25 @@
 'use strict'
 
 import React, { Component } from 'react'
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View
-} from 'react-native'
-import Button from 'react-native-button'
+import ScreenWithInput from './ScreenWithInput'
 
 class LoginEmailScreen extends Component {
   render() {
+    const inputProps = {
+      keyboardType: 'email-address',
+      returnKeyType: 'next',
+      onChangeText: (text) => { this.props.onEnterEmail(text) },
+      onSubmitEditing: this.props.onSubmitPress,
+      defaultValue: this.props.email
+    }
+
     return (
-      <View style={styles.container}>
-        <View style={styles.labelSection}>
-          <Text style={styles.label}>
-            What's your email?
-          </Text>
-        </View>
-        <View style={styles.inputSection}>
-          <TextInput
-            style={styles.inputField}
-            autoCapitalize='none'
-            autoCorrect={false}
-            autoFocus={true}
-            keyboardType='email-address'
-            onChangeText={(text) => this.props.onEnterEmail(text)}
-            onSubmitEditing={this.props.onSubmitPress}
-            returnKeyType='next'
-            defaultValue={this.props.email}
-          />
-        </View>
-      </View>
+      <ScreenWithInput
+        label="What's your email?"
+        inputProps={inputProps}
+      />
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffe945',
-  },
-  labelSection: {
-    flex: 1,
-    justifyContent: 'flex-end'
-  },
-  label: {
-    fontSize: 24,
-    textAlign: 'center',
-    color: '#333333',
-    fontWeight: '300',
-    marginBottom: 20
-  },
-  inputSection: {
-    flex: 1
-  },
-  inputField: {
-    width: 250,
-    height: 40,
-    borderColor: '#333333',
-    borderWidth: 1,
-    color: '#333333',
-    fontWeight: '100',
-    fontSize: 16,
-    backgroundColor: 'rgba(255, 255, 255, 0.85)',
-    padding:5
-  }
-})
 
 export default LoginEmailScreen
--- app/components/LoginPasswordScreen.js
+++ app/components/LoginPasswordScreen.js
@@ -1,88 +1,26 @@
 'use strict'
 
 import React, { Component } from 'react'
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View
-} from 'react-native'
-import Button from 'react-native-button'
+import ScreenWithInput from './ScreenWithInput'
 
 class LoginPasswordScreen extends Component {
-  constructor(props, context) {
-    super(props, context)
-
-    this.state = {
-      password: ''
-    }
-  }
-
   render() {
+    const inputProps = {
+      keyboardType: 'email-address',
+      returnKeyType: 'go',
+      onChangeText: (text) => { this.props.onEnterPassword(text) },
+      onSubmitEditing: this.props.onSubmitPress,
+      secureTextEntry: true
+    }
+
     return (
-      <View style={styles.container}>
-        <View style={styles.labelSection}>
-          <Text style={styles.label}>
-            What's your password?
-          </Text>
-        </View>
-        <View style={styles.inputSection}>
-          <TextInput
-            style={styles.inputField}
-            autoCapitalize='none'
-            autoCorrect={false}
-            autoFocus={true}
-            onChangeText={(text) => this.props.onEnterPassword(text)}
-            onSubmitEditing={this.props.onSubmitPress}
-            returnKeyType='go'
-            secureTextEntry={true}
-          />
-          <Text style={styles.errorMessage}>
-            {this.props.error}
-          </Text>
-        </View>
-      </View>
+      <ScreenWithInput
+        error={this.props.error}
+        label="What's your password?"
+        inputProps={inputProps}
+      />
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffe945',
-  },
-  labelSection: {
-    flex: 1,
-    justifyContent: 'flex-end'
-  },
-  label: {
-    fontSize: 24,
-    textAlign: 'center',
-    color: '#333333',
-    fontWeight: '300',
-    marginBottom: 20
-  },
-  inputSection: {
-    flex: 1
-  },
-  inputField: {
-    width: 250,
-    height: 40,
-    borderColor: '#333333',
-    borderWidth: 1,
-    color: '#333333',
-    fontWeight: '100',
-    fontSize: 16,
-    backgroundColor: 'rgba(255, 255, 255, 0.85)',
-    padding:5
-  },
-  errorMessage: {
-    fontSize: 16,
-    margin: 10,
-    textAlign: 'center'
-  }
-})
 
 export default LoginPasswordScreen
