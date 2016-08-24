0022d2ae4dd6def8448f5b677e73f132333a3038
Herman Ng <heymanhn@gmail.com>
2016-08-24 11:24:01 -0700

    Add React Native logger middleware

--- app/app.js
+++ app/app.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
 
 import reducers from './reducers'
 import AppContainer from './containers/AppContainer'
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+const loggerMiddleware = createLogger()
+const createStoreWithMiddleware = applyMiddleware(
+  thunkMiddleware,
+  loggerMiddleware
+)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
 export default class App extends Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<AppContainer />
-			</Provider>
-		)
-	}
+  render() {
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    )
+  }
 }
