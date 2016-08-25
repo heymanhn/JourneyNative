48bf9b11c0d05ce970009fe5229693eaf431ff9d
Herman Ng <heymanhn@gmail.com>
2016-08-24 20:47:36 -0700

    Use redux-persist to save redux store to AsyncStorage
    This allows the user's session to be restored upon force close + restart
    of the app. However, the app still animates a navigation to return to
    the Trips page. Need to address that in a follow-up commit.
    
    [#129003891]

--- app/app.js
+++ app/app.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { AsyncStorage } from 'react-native'
 import { Provider } from 'react-redux'
 import createLogger from 'redux-logger'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import thunkMiddleware from 'redux-thunk'
 
 import reducers from './reducers'
 import AppContainer from './containers/AppContainer'
 
 const loggerMiddleware = createLogger()
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware,
-  loggerMiddleware
-)(createStore)
-const store = createStoreWithMiddleware(reducers)
+const store = compose(
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  ),
+  autoRehydrate()
+)(createStore)(reducers)
+persistStore(store, { storage: AsyncStorage })
 
 export default class App extends Component {
   render() {
